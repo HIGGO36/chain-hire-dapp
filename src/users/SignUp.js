92e3e0d8c22d1516caf9e99717d7f9209a2c92c8
John D. Higgins <stuffadmin@protonmail.com>
2024-02-19 21:41:44 -0500

    added cryptographic library bcrypt.js to hash the passwords before storing them.

--- src/users/SignUp.js
+++ src/users/SignUp.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { db } from './access/firebase/firebaseConfig';
 import { collection, addDoc } from 'firebase/firestore';
+import bcrypt from 'bcryptjs'; // Import bcrypt for password hashing
 import Avatar from '@mui/material/Avatar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
@@ -20,11 +21,9 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Grid from '@mui/material/Grid';
 import { verifyBusinessEmail } from '../utils/verifyBusinessEmail';
 
-
 const DefaultTheme = createTheme();
 
 export default function SignUp() {
-
   const [userType, setUserType] = useState('Job Seeker');
 
   const [userData, setUserData] = useState({
@@ -38,6 +37,7 @@ export default function SignUp() {
     businessEmail: '',
     businessPhone: '',
   });
+
   const [alertInfo, setAlertInfo] = useState({ message: '', severity: '' });
 
   const handleChange = (event) => {
@@ -45,97 +45,97 @@ export default function SignUp() {
     setUserData({ ...userData, [name]: value });
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-const handleSubmit = async (event) => {
-  event.preventDefault();
+    // Reset previous alerts
+    setAlertInfo({ message: '', severity: 'info' });
 
-  // Reset previous alerts
-  setAlertInfo({ message: '', severity: 'info' });
+    let errorMessage = [];
 
-  let errorMessage = [];
-  // Validate personal email
-  if (!validateEmail(userData.email)) {
-    errorMessage.push('a valid personal email address');
-  }
+    // Validate personal email
+    if (!validateEmail(userData.email)) {
+      errorMessage.push('a valid personal email address');
+    }
 
-  // Validate phone based on userType and add specific format instruction if invalid
-  if (userType !== 'Job Seeker' && !validatePhone(userData.businessPhone)) {
-    errorMessage.push('a valid phone number in the format 123-456-7890');
-  }
+    // Validate phone based on userType and add specific format instruction if invalid
+    if (userType !== 'Job Seeker' && !validatePhone(userData.businessPhone)) {
+      errorMessage.push('a valid phone number in the format 123-456-7890');
+    }
 
-  // Attempt to verify the business email for Employer and Recruiter, if applicable
-  if (userType === 'Employer' || userType === 'Recruiter') {
-    try {
-      const verificationResult = await verifyBusinessEmail(userData.businessEmail);
-      if (!verificationResult.success) {
-        errorMessage.push(verificationResult.message);
+    // Attempt to verify the business email for Employer and Recruiter, if applicable
+    if (userType === 'Employer' || userType === 'Recruiter') {
+      try {
+        const verificationResult = await verifyBusinessEmail(userData.businessEmail);
+        if (!verificationResult.success) {
+          errorMessage.push(verificationResult.message);
+        }
+      } catch (error) {
+        console.error("Error verifying business email:", error);
+        errorMessage.push('Failed to verify business email. Please try again.');
       }
+    }
+
+    // If there are any validation or verification errors, show them and stop the form submission
+    if (errorMessage.length > 0) {
+      setAlertInfo({
+        message: `Please correct the following: ${errorMessage.join(', ')}.`,
+        severity: 'error',
+      });
+      return;
+    }
+
+    // Proceed with user creation process if validations and verifications pass
+    try {
+      const auth = getAuth();
+      const hashedPassword = await bcrypt.hash(userData.password, 10); // Hash the password
+      const userCredential = await createUserWithEmailAndPassword(auth, userData.email, hashedPassword);
+      const user = userCredential.user;
+
+      let collectionName = '';
+      switch (userType) {
+        case 'Employer':
+          collectionName = 'employers';
+          break;
+        case 'Recruiter':
+          collectionName = 'recruiters';
+          break;
+        default:
+          collectionName = 'jobseekers';
+      }
+
+      await addDoc(collection(db, collectionName), {
+        ...userData,
+        uid: user.uid,
+        createdAt: new Date(),
+      });
+
+      setAlertInfo({
+        message: `Welcome aboard Chain Hire platform as a ${userType}, you are now successfully signed up!`,
+        severity: 'success',
+      });
     } catch (error) {
-      console.error("Error verifying business email:", error);
-      errorMessage.push('Failed to verify business email. Please try again.');
+      console.error("Error signing up:", error);
+      setAlertInfo({
+        message: error.message,
+        severity: 'error',
+      });
     }
+  };
+
+  // Correct email regex pattern
+  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  // Function to check if the email is valid
+  function validateEmail(email) {
+    return emailPattern.test(email);
   }
 
-  // If there are any validation or verification errors, show them and stop the form submission
-  if (errorMessage.length > 0) {
-    setAlertInfo({
-      message: `Please correct the following: ${errorMessage.join(', ')}.`,
-      severity: 'error',
-    });
-    return;
+  // Utility function to validate phone number (US format)
+  function validatePhone(phone) {
+    const re = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    return re.test(String(phone));
   }
-
-  // Proceed with user creation process if validations and verifications pass
-  try {
-    const auth = getAuth();
-    const userCredential = await createUserWithEmailAndPassword(auth, userData.email, userData.password);
-    const user = userCredential.user;
-
-    let collectionName = '';
-    switch (userType) {
-      case 'Employer':
-        collectionName = 'employers';
-        break;
-      case 'Recruiter':
-        collectionName = 'recruiters';
-        break;
-      default:
-        collectionName = 'jobseekers';
-    }
-
-    await addDoc(collection(db, collectionName), {
-      ...userData,
-      uid: user.uid,
-      createdAt: new Date(),
-    });
-
-    setAlertInfo({
-      message: `Welcome aboard Chain Hire platform as a ${userType}, you are now successfully signed up!`,
-      severity: 'success',
-    });
-  } catch (error) {
-    console.error("Error signing up:", error);
-    setAlertInfo({
-      message: error.message,
-      severity: 'error',
-    });
-  }
-};
-
-
-// Correct email regex pattern
-const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-// Function to check if the email is valid
-function validateEmail(email) {
-  return emailPattern.test(email);
-}
-
-// Utility function to validate phone number (US format)
-function validatePhone(phone) {
-  const re = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
-  return re.test(String(phone));
-}
 
   const renderUserTypeFields = () => {
     switch (userType) {
@@ -192,114 +192,115 @@ function validatePhone(phone) {
                 required
                 fullWidth
                 name="positionTitle"
-                label="Position Title"
+                label="
+                Position Title"
                 value={userData.positionTitle}
                 onChange={handleChange}
                 helperText="HR Manager"
-              />
-            </Grid>
-             <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Business Email"
-                name="businessEmail"
-                type="email"
-                value={userData.businessEmail}
-                onChange={handleChange}
-                helperText="sarahsmith@microsoft.com"
-              />
-            </Grid>
-             <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id="businessPhone"
-                label="Business Phone"
-                name="businessPhone"
-                type="tel"
-                inputProps={{ 
-                pattern: "[0-9]{3}-[0-9]{3}-[0-9]{4}" // Example pattern for US phone numbers: 123-456-7890
-                }}
-                value={userData.businessPhone}
-                onChange={handleChange}
-                helperText="Format: 123-456-7890"
-              /> 
-            </Grid>
-          </>
-        );
-      case 'Job Seeker':
-      default:
-        return null;
-    }
-  };
-
-  return (
-    <ThemeProvider theme={DefaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box sx={{ marginTop: "100px", display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">Sign up</Typography>
-          <FormControl component="fieldset" sx={{ mt: 3 }}>
-            <FormLabel component="legend">User Type</FormLabel>
-            <RadioGroup row name="userType" value={userType} onChange={(e) => setUserType(e.target.value)}>
-              <FormControlLabel value="Job Seeker" control={<Radio />} label="Job Seeker" />
-         
-              <FormControlLabel value="Employer" control={<Radio />} label="Employer" />
-              <FormControlLabel value="Recruiter" control={<Radio />} label="Recruiter" />
-            </RadioGroup>
-          </FormControl>
-          {alertInfo.message && (
-            <Alert severity={alertInfo.severity} sx={{ width: '100%', mt: 2 }}>
-              {alertInfo.message}
-            </Alert>
-          )}
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  type="email"
-                  name="email"
-                  autoComplete="email"
-                  value={userData.email}
-                  onChange={handleChange}
-                  helperText="mickeymouse@gmail.com"
-                />             
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  value={userData.password}
-                  onChange={handleChange}
                 />
-              </Grid>
-              {renderUserTypeFields()}
-            </Grid>
-            <Button
-              type="submit"
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                <TextField
+             required
+             fullWidth
+             id="email"
+             label="Business Email"
+             name="businessEmail"
+             type="email"
+             value={userData.businessEmail}
+             onChange={handleChange}
+             helperText="sarahsmith@microsoft.com"
+             />
+</Grid>
+<Grid item xs={12} sm={6}>
+<TextField
+required
+fullWidth
+id="businessPhone"
+label="Business Phone"
+name="businessPhone"
+type="tel"
+inputProps={{
+pattern: "[0-9]{3}-[0-9]{3}-[0-9]{4}" // Example pattern for US phone numbers: 123-456-7890
+}}
+value={userData.businessPhone}
+onChange={handleChange}
+helperText="Format: 123-456-7890"
+/>
+</Grid>
+</>
+);
+case 'Job Seeker':
+default:
+return null;
+}
+};
+
+return (
+<ThemeProvider theme={DefaultTheme}>
+<Container component="main" maxWidth="xs">
+<CssBaseline />
+<Box sx={{ marginTop: "100px", display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+<LockOutlinedIcon />
+</Avatar>
+<Typography component="h1" variant="h5">Sign up</Typography>
+<FormControl component="fieldset" sx={{ mt: 3 }}>
+<FormLabel component="legend">User Type</FormLabel>
+<RadioGroup row name="userType" value={userType} onChange={(e) => setUserType(e.target.value)}>
+ <FormControlLabel value="Job Seeker" control={<Radio />} label="Job Seeker" />
+            
+          <FormControlLabel value="Employer" control={<Radio />} label="Employer" />
+          <FormControlLabel value="Recruiter" control={<Radio />} label="Recruiter" />
+        </RadioGroup>
+      </FormControl>
+      {alertInfo.message && (
+        <Alert severity={alertInfo.severity} sx={{ width: '100%', mt: 2 }}>
+          {alertInfo.message}
+        </Alert>
+      )}
+      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              required
               fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+              id="email"
+              label="Email Address"
+              type="email"
+              name="email"
+              autoComplete="email"
+              value={userData.email}
+              onChange={handleChange}
+              helperText="mickeymouse@gmail.com"
+            />             
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="new-password"
+              value={userData.password}
+              onChange={handleChange}
+            />
+          </Grid>
+          {renderUserTypeFields()}
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign Up
+        </Button>
+      </Box>
+    </Box>
+  </Container>
+</ThemeProvider>
+);
 }
