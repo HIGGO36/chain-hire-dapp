88ef6af29adcf743b5681174336e8c0c2549981e
John D. Higgins <stuffadmin@protonmail.com>
2024-03-06 22:34:57 -0500

    updated EmployerPortal incorporates persistence by saving the latest minted NFT's details to . Upon component initialization, it attempts to load this data to display the most recent NFT, maintaining its visibility even after page refreshes.

--- src/users/profile/components/EmployerPortal.js
+++ src/users/profile/components/EmployerPortal.js
@@ -7,50 +7,20 @@ import { ethers } from 'ethers';
 import JobRoleNFTv5ABI from '../../utils/abis/JobRoleNFTv5ABI.json';
 
 const EmployerPortal = () => {
-  const [userAddress, setUserAddress] = useState('');
-  const [latestTokenId, setLatestTokenId] = useState(null);
-  const [latestNFT, setLatestNFT] = useState(null);
+const [userAddress, setUserAddress] = useState('');
+const [latestNFT, setLatestNFT] = useState(() => {
+const savedNFT = localStorage.getItem('latestNFT');
+    return savedNFT ? JSON.parse(savedNFT) : null;
+  });
   const navigate = useNavigate();
   const contractAddress = process.env.REACT_APP_JOB_ROLE_NFT_V5_CONTRACT_ADDRESS;
 
   useEffect(() => {
-    const storedAddress = localStorage.getItem('userAddress');
-    if (storedAddress) {
-      setUserAddress(storedAddress);
-    }
+  const storedAddress = localStorage.getItem('userAddress');
+  if (storedAddress) {
+    setUserAddress(storedAddress);
+  }
   }, []);
-
-  useEffect(() => {
-    // This function remains mostly the same
-    const fetchNFTDetails = async () => {
-      if (latestTokenId) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const contract = new ethers.Contract(contractAddress, JobRoleNFTv5ABI, provider);
-        const nftData = await contract.getJobRoleData(latestTokenId);
-        
-        // Process and set the NFT data with correct formatting for BigNumber objects
-        const formattedNftData = {
-          tokenId: latestTokenId.toString(),
-          companyName: nftData.companyName,
-          industry: nftData.industry,
-          jobRoleTitle: nftData.jobRoleTitle,
-          minSalary: ethers.utils.formatEther(nftData.minSalary),
-          maxSalary: ethers.utils.formatEther(nftData.maxSalary),
-          workLocationType: nftData.workLocationType,
-          country: nftData.country,
-          location: nftData.location,
-          positionSummary: nftData.positionSummary,
-          responsibilities: nftData.responsibilities,
-          qualifications: nftData.qualifications,
-          lifeSpan: nftData.lifeSpan.toString(),
-        };
-        
-        setLatestNFT(formattedNftData);
-      }
-    };
-
-    fetchNFTDetails();
-  }, [latestTokenId, contractAddress]);
 
   const handleConnect = (account) => {
     setUserAddress(account);
@@ -65,45 +35,67 @@ const EmployerPortal = () => {
     navigate('/MarketplaceDashboard');
   };
 
- const handleTokenMinted = (tokenId) => {
-    setLatestTokenId(tokenId);
-    // This triggers the useEffect to fetch and format the latest minted NFT details
+  const handleTokenMinted = (tokenId) => {
+  const fetchNFTDetails = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const contract = new ethers.Contract(contractAddress, JobRoleNFTv5ABI, provider);
+  const nftData = await contract.getJobRoleData(tokenId);
+
+  const formattedNftData = {
+  tokenId: tokenId.toString(),
+  companyName: nftData.companyName,
+  industry: nftData.industry,
+  jobRoleTitle: nftData.jobRoleTitle,
+  minSalary: ethers.utils.formatEther(nftData.minSalary),
+  maxSalary: ethers.utils.formatEther(nftData.maxSalary),
+  workLocationType: nftData.workLocationType,
+  country: nftData.country,
+  location: nftData.location,
+  positionSummary: nftData.positionSummary,
+  responsibilities: nftData.responsibilities,
+  qualifications: nftData.qualifications,
+  lifeSpan: nftData.lifeSpan.toString(),
   };
 
- return (
-   <div style={{ position: 'relative', margin: '0 auto', width: '99%', maxWidth: '800px', height: '550px', backgroundColor: '#20336B', border: '10px solid #0E1D47', borderRadius: '10%', boxShadow: '0 0 20px rgba(255, 255, 255, 0.5)', color: '#D4E774' }}>
-     
-      {latestNFT && (
-       <Card style={{ position: 'absolute', top: '12%', left: '36%', padding: '10px', marginTop: '20px', backgroundColor: '#D7E5EB', color: '#031B25', zIndex: '9999' }}>
-                <Typography variant="h6" style={{fontWeight: '650'}}>Last Job Role Minted</Typography>
-                <Typography>Token ID: {latestNFT.tokenId}</Typography>
-                <Typography>Company: {latestNFT.companyName}</Typography>
-                <Typography>Industry: {latestNFT.industry}</Typography>
-                <Typography>Job Role: {latestNFT.jobRoleTitle}</Typography>
-                <Typography>Min Salary: {latestNFT.minSalary} ETH</Typography>
-                <Typography>Max Salary: {latestNFT.maxSalary} ETH</Typography>
-                <Typography>Location: {latestNFT.location}</Typography>
-                <Typography>Responsibilities: {latestNFT.responsibilities}</Typography>
-                <Typography>Qualifications: {latestNFT.qualifications}</Typography>
-                <Typography>Life Span: {latestNFT.lifeSpan} days</Typography>
-              </Card>
-     )}
-     
-      <div style={{ position: 'absolute', top: '230px', left: '90px', transform: 'translate(-50%, -50%)', display: 'flex', flexDirection: 'column', gap: '20px' }}>
-        <MetaMaskConnectButton style={{ position: 'fixed', top: '-60px', left: '8px', fontSize: '14px', color: '#D4E774' }} onConnect={handleConnect} />
-        {userAddress && (
-          <>
-            <MintJobRoleNFTButton userAddress={userAddress} onTokenMinted={handleTokenMinted} />
-            <Button style={{ fontSize: '17px', backgroundColor: '#808183', border: '2px solid #113171', color: '#D4E774', textAlign: 'left' }} onClick={navigateToWallet}>COLLECTION</Button>
-            {/* Removed BURN button for clarity, assuming it's not implemented here */}
-            <Button style={{ fontSize: '17px', backgroundColor: '#808183', border: '2px solid #113171', color: '#D4E774', textAlign: 'left' }} onClick={navigateToMarketplace}>MARKETPLACE</Button>
-            {/* Removed MANAGE LISTINGS button for clarity, assuming it's not implemented here */}
-           
-          </>
-        )}
-      </div>
-    </div>
-  );
-};
+  setLatestNFT(formattedNftData);
+  localStorage.setItem('latestNFT', JSON.stringify(formattedNftData));
+  };
 
+  fetchNFTDetails();
+  };
+
+return (
+  <div style={{ position: 'relative', margin: '0 auto', width: '99%', maxWidth: '800px', height: '550px', backgroundColor: '#20336B', border: '10px solid #0E1D47', borderRadius: '10%', boxShadow: '0 0 20px rgba(255, 255, 255, 0.5)', color: '#D4E774' }}>
+
+{latestNFT && (
+<Card style={{ position: 'absolute', top: '12%', left: '36%', padding: '10px', marginTop: '20px', backgroundColor: '#D7E5EB', color: '#031B25', zIndex: '9999' }}>
+<Typography variant="h6" style={{fontWeight: '650'}}>Last Minted: Job Role</Typography>
+<Typography>Token ID: {latestNFT.tokenId}</Typography>
+<Typography>Company: {latestNFT.companyName}</Typography>
+<Typography>Industry: {latestNFT.industry}</Typography>
+<Typography>Job Role: {latestNFT.jobRoleTitle}</Typography>
+<Typography>Min Salary: {latestNFT.minSalary} ETH</Typography>
+<Typography>Max Salary: {latestNFT.maxSalary} ETH</Typography>
+<Typography>Location Type: {latestNFT.workLocationType}</Typography>
+<Typography>Country: {latestNFT.country}</Typography>
+<Typography>Location: {latestNFT.location}</Typography>
+<Typography>Position Summary: {latestNFT.positionSummary}</Typography>
+<Typography>Responsibilities: {latestNFT.responsibilities}</Typography>
+<Typography>Qualifications: {latestNFT.qualifications}</Typography>
+<Typography>Life Span: {latestNFT.lifeSpan} days</Typography>
+</Card>
+)}
+<div style={{ position: 'absolute', top: '230px', left: '90px', transform: 'translate(-50%, -50%)', display: 'flex', flexDirection: 'column', gap: '20px' }}>
+<MetaMaskConnectButton style={{ position: 'fixed', top: '-60px', left: '8px', fontSize: '14px', color: '#D4E774' }} onConnect={handleConnect} />
+{userAddress && (
+<>
+<MintJobRoleNFTButton userAddress={userAddress} onTokenMinted={handleTokenMinted} />
+<Button style={{ fontSize: '17px', backgroundColor: '#808183', border: '2px solid #113171', color: '#D4E774', textAlign: 'left' }} onClick={navigateToWallet}>COLLECTION</Button>
+<Button style={{ fontSize: '17px', backgroundColor: '#808183', border: '2px solid #113171', color: '#D4E774', textAlign: 'left' }} onClick={navigateToMarketplace}>MARKETPLACE</Button>       
+</>
+)}
+</div>
+</div>
+);
+};
 export default EmployerPortal;
